d5ea8d77859cdb99d91cb76bb50b177cc2c32964
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2021-04-07 15:56:42 +0200

    Exception handling instead of active checking

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from runtime import Runtime
 
 # main program if this file get executed
 if __name__ == '__main__':
-    app = Runtime()
+    while True:
+        app = Runtime()
 
-    try:
-        app.loop()
-    except KeyboardInterrupt:  # shutdown python program gently
-        print('Stopped with KeyboardInterrupt!')
-    finally:
-        GPIO.cleanup()  # cleanup GPIOs (to avoid warning on next startup)
-        app.program_stopped.set()
-        # Exiting message
-        print("6-RUS program was terminated due to user-input or an error (Please wait ca. 5s)")
-        print("Please start the program again to control the robot again!")
+        try:
+            app.loop()
+        except KeyboardInterrupt:  # shutdown python program gently
+            print('Stopped with KeyboardInterrupt!')
+            break
+        finally:
+            GPIO.cleanup()  # cleanup GPIOs (to avoid warning on next startup)
+            app.program_stopped.set()
+            # Exiting message
+            print("6-RUS program was terminated due to user-input or an error (Please wait ca. 5s)")
+            print("Please start the program again to control the robot again!")
