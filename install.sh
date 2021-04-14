367dee8000e06e6200311d6ffdbf418372638d7a
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2021-04-14 15:49:41 +0200

    Adding working (!) autostart configuration

--- install.sh
+++ install.sh
@@ -4,3 +4,10 @@ sudo apt-get install libatlas-base-dev -y
 
 # Install python dependencies
 python3 -m pip install ./6RUSRobot/requirements.txt
+
+# Create autostart directory
+sudo mkdir /home/pi/.config/autostart/ -p
+
+# Copy file to autostart directory
+sudo cp ./mini_parallel.desktop /home/pi/.config/autostart/mini_parallel.desktop
+
