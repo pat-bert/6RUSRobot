245b0097cbac18d41b6042766b3eaba775ea0531
Patrick <47899331+pat-bert@users.noreply.github.com>
2021-05-14 09:54:48 +0200

    Fixing wrong index in CLI arguments Safe-guarding against I2C error

--- display.py
+++ display.py
@@ -3,7 +3,7 @@ from typing import List
 from RPLCD.i2c import CharLCD
 
 
-class LCD(CharLCD):
+class LCD:
     AXIS = 'XYZABC'
     POST_DECIMAL_PLACE = 1
     PRE_DECIMAL_PLACE = 3
@@ -38,35 +38,47 @@ class LCD(CharLCD):
     def __init__(self):
         # Adress and port expander type are fixed
         # Hide the specific implementation used
-        super().__init__('PCF8574', 0x26)
-        self.create_char(self.CONNECTED_CHAR, self.CONNECTED_SYMBOL)
-        self.create_char(self.DISCONNECTED_CHAR, self.DISCONNECTED_SYMBOL)
+        try:
+            self._lcd = CharLCD('PCF8574', 0x26)
+        except Exception:
+            self.connected = False
+        else:
+            self.connected = True
+            self._lcd.create_char(self.CONNECTED_CHAR, self.CONNECTED_SYMBOL)
+            self._lcd.create_char(self.DISCONNECTED_CHAR, self.DISCONNECTED_SYMBOL)
 
     def print_pose(self, pose: List[float]):
         """
         Prints a pose on the lower three rows of the display
         """
-        rows = 3 * ['']
+        if self.connected:
+            rows = 3 * ['']
 
-        for i, (ax, val) in enumerate(zip(self.AXIS, pose)):
-            space = ' ' if i // 3 == 0 else ''
-            rows[i % 3] += f'{ax}{val:+0{self.WIDTH}.{self.POST_DECIMAL_PLACE}f}{space}'
+            for i, (ax, val) in enumerate(zip(self.AXIS, pose)):
+                space = ' ' if i // 3 == 0 else ''
+                rows[i % 3] += f'{ax}{val:+0{self.WIDTH}.{self.POST_DECIMAL_PLACE}f}{space}'
 
-        display_str = '\r\n'.join(rows)
+            display_str = '\r\n'.join(rows)
 
-        # Set cursor to start of second row and write positions
-        self.cursor_pos = (1, 0)
-        self.write_string(display_str)
+            # Set cursor to start of second row and write positions
+            self._lcd.cursor_pos = (1, 0)
+            self._lcd.write_string(display_str)
 
     def print_connection(self, is_connected: bool):
-        # Move cursor to upper right corner
-        self.cursor_pos = (0, 19)
+        if self.connected:
+            # Move cursor to upper right corner
+            self._lcd.cursor_pos = (0, 19)
 
-        if is_connected:
-            self.write_string(chr(self.CONNECTED_CHAR))
-        else:
-            self.write_string(chr(self.DISCONNECTED_CHAR))
+            if is_connected:
+                self._lcd.write_string(chr(self.CONNECTED_CHAR))
+            else:
+                self._lcd.write_string(chr(self.DISCONNECTED_CHAR))
 
     def print_status(self, status: str):
-        self.home()
-        self.write_string(status[:19])
+        if self.connected:
+            self._lcd.home()
+            self._lcd.write_string(status[:19])
+
+    def __del__(self):
+        if self.connected:
+            self._lcd.close()
--- main.py
+++ main.py
@@ -7,11 +7,19 @@ from runtime import Runtime
 # main program if this file get executed
 if __name__ == '__main__':
     logging.basicConfig(filename='log.txt', level=logging.DEBUG)
-    app = Runtime(argv[0])
+
     try:
+        robot_type = argv[1]
+    except IndexError:
+        logging.exception("Need to supply robot type as command-line argument")
+        raise
+
+    app = Runtime(robot_type)
+    try:
+        app_initialized = True
         app.loop()
     except KeyboardInterrupt:  # shutdown python program gently
-        print('Stopped with KeyboardInterrupt!')
+        print("Stopped with KeyboardInterrupt!")
     except Exception as e:
         logging.exception(e)
     finally:
